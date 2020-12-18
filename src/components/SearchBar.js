d34ee1261b3a0c5335afee2a6c9aab5dc468b918
amirulraziq <amirulraziqi@gmail.com>
2020-12-19 01:49:17 +0800

    Pass prop from SearchScreen to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name='search' style={styles.iconStyle} />
             <TextInput 
-                placeholder='Search'
                 style={styles.inputStyle}
+                placeholder='Search'
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
+                autoCapitalize='none'
+                autoCorrect={false}
             />
         </View>       
     )
