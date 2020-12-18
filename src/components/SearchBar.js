9b40492acc72453292b38ddb97e5a0b546fcda25
amirulraziq <amirulraziqi@gmail.com>
2020-12-19 01:24:50 +0800

    imported SearchBar into SearchScreen

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const SearchBar = () => {
     return (
-        <View>
-            <Text>Search Bar</Text>
+        <View style={styles.background}>
+            <Text>Search</Text>
         </View>       
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+    }
+});
 
 export default SearchBar;
