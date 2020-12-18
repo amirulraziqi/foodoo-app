83de80eaf27188dcd1fc43e8e64d485e95b7c66e
amirulraziq <amirulraziqi@gmail.com>
2020-12-19 03:17:55 +0800

    setting up GET request to Yelp API

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,13 +1,12 @@
 // We can use axios DIRECTLY to make a network request.
 // Alternatively, we can make an instance of it that has pre-set options assigned
 
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        // to Authorize with Yelp API
-        Authorization: 'Bearer nBi09Kmjbx8rb4HU1wGv6dbdS8_aJINFJkYA8EEOabPrN8dYHxe7DTczPk-S028acEReDXXfmXFeG8y_h1RrnX_fqx0_7QGffuDphhW3UKtmK7IFMhzK1WH3rNTcX3Yx'
-    }
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      'Bearer nBi09Kmjbx8rb4HU1wGv6dbdS8_aJINFJkYA8EEOabPrN8dYHxe7DTczPk-S028acEReDXXfmXFeG8y_h1RrnX_fqx0_7QGffuDphhW3UKtmK7IFMhzK1WH3rNTcX3Yx',
+  }
 });
-
