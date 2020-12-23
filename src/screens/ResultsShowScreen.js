2525d3c841b946fb79f7c1d841fc4b7763dfb9bb
amirulraziq <amirulraziqi@gmail.com>
2020-12-24 07:01:32 +0800

    Render business food photos on ResultsShowScreen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,17 +1,47 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
 
-    console.log(id)
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    console.log(result);
+
+    if (!result) {
+        return null;
+    }
+
     return (
         <View>
-            <Text>Results Show</Text>
+            <Text>{result.name}</Text>
+            <FlatList 
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({ item }) => {
+                    return <Image source={{ uri: item }} style={styles.imageStyle}/>
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: 300,
+        height: 200,
+        borderRadius: 4,
+        marginBottom: 10,
+    }
+});
 
 export default ResultsShowScreen;
